1988cc8d22f9383f4e80b0dceefcef0ca220a424
default <default@local.mac>
2016-08-07 18:18:48 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,64 @@
+
+## Usage
+
+```
+npm install -g hexo
+npm install
+```
+
+## create new post
+
+```
+hexo new post-example
+```
+
+then edit `source/_post/post-example.md`
+
+## local development
+
+```
+hexo generate -f
+hexo server -o
+```
+
+## deploy to github pages
+
+```
+npm run deploy
+```
+
+## config
+
+There are some values you should change.
+
+**_config.yaml**
+
+```
+title: Jane
+description: website description
+author: Jane
+
+url: https://username.github.io
+root: /repo/
+
+# Deployment
+## Docs: https://hexo.io/docs/deployment.html
+deploy:
+  type: git
+  repo: git@github.com:username/repo.git // your github repo
+  branch: gh-pages
+
+```
+
+**themes/jane/_config.yml**
+
+```
+enable_links: false // you can enable or disable links under title
+
+duoshuo_shortname:
+disqus_shortname:
+
+google_analytics:
+favicon: /images/fav.ico
+
+```
---
+++ gh_pages.js
@@ -0,0 +1,63 @@
+import 'shelljs/global';
+import Git from 'git-repository';
+import SimpleGit from 'simple-git';
+import path from 'path';
+import YAML from 'yamljs';
+import fs from 'fs';
+
+const yaml = YAML.parse(fs.readFileSync(path.join(__dirname, '_config.yml'), { encoding: 'utf8' }));
+
+const conf = {
+	url: yaml.deploy.repo,
+	name: 'origin',
+	branch: yaml.deploy.branch
+};
+
+const public_path = path.join(__dirname, 'public');
+
+function run(cmd) {
+	return new Promise((resolve, reject) => {
+		exec(cmd, (code, output) => {
+			if (code !== 0) {
+				return reject(code);
+			}
+			resolve();
+		});
+	});
+}
+
+async function main() {
+	try {
+		try {
+			const stat = fs.statSync(public_path);
+			if (!stat.isDirectory()) {
+				fs.mkdirSync(public_path);
+			}
+		} catch (e) {
+			fs.mkdirSync(public_path);
+		}
+		const repo = await Git.open(public_path, { init: true });
+		await repo.setRemote(conf.name, conf.url);
+		// Fetch the remote repository if it exists
+		if ((await repo.hasRef(conf.url, conf.branch))) {
+			await repo.fetch(conf.name);
+			await repo.reset(`${conf.name}/${conf.branch}`, { hard: true });
+			await repo.clean({ force: true });
+		}
+
+		const simpleGit = SimpleGit(public_path);
+  	await simpleGit.checkout(conf.branch);
+
+  	await run(`hexo generate -f`);
+
+  	await repo.add('--all .');
+  	await repo.commit(`${conf.branch} update`);
+  	await repo.push(conf.name, conf.branch, { force: true });
+
+	} catch (e) {
+		console.log('error =', e, e.stack);
+	}
+	process.exit(0);
+}
+
+main();
